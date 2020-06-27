84225439cdcdd3e0b834bfbf57656c86120431d5
Ozan <ozan.aydin@alo-tech.com>
2020-06-27 22:32:11 +0300

    Updates

---
+++ precache-manifest.50c9f42a8d13409d15dcb271446698e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a7e8fab478506b0ec11d84bfd45012",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12638c8bbf2326fd1649",
+    "url": "./static/js/2.6d533ce4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6d533ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dbef75292a6e7b8747",
+    "url": "./static/js/main.95c5f50f.chunk.js"
+  },
+  {
+    "revision": "f7e40da96d09fba207b7",
+    "url": "./static/js/runtime-main.632d956e.js"
+  }
+]);
